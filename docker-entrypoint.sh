9e957a77396f6db22ae3a23b9e2520b75a5a9622
germanrcuriel <germix@germix.net>
2018-02-26 19:59:00 +0100

    Use parent image entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "Starting Assetto Corsa Server..."
+echo ">>> Updating Assetto Corsa Dedicated Server..."
+install-steam-app.sh
+
+echo ">>> Starting Assetto Corsa Dedicated Server..."
 cd /steamapps/${STEAM_APP_NAME:=$STEAM_APP_ID}
 ./acServer
