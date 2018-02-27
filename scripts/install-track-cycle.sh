d2327a0c83e7e0f8d8249f6f55afc2ca7f08bfb4
germanrcuriel <germix@germix.net>
2018-02-27 16:50:31 +0100

    Update track-cycle default config

--- scripts/install-track-cycle.sh
+++ scripts/install-track-cycle.sh
@@ -4,6 +4,13 @@ TRACK_CYCLE_PATH="$INSTALL_PATH/plugins/track-cycle"
 
 cd /opt/track-cycle
 
-if [ ! -f $TRACK_CYCLE_PATH ]; then
+if [ ! -d $TRACK_CYCLE_PATH ]; then
+  cp AC_TrackCycle_Console.exe.config AC_TrackCycle_Console.exe.config_DEFAULT
+  sed -i "
+    s#<add key=\"ac_server_directory\" value=\"\"/>#<add key=\"ac_server_directory\" value=\"../../\"/>#
+    s#<add key=\"welcome_message\".*#<add key=\"welcome_message\" value=\"${TC_WELCOME_MESSAGE}\" />#
+    /<appSettings>/a \    <add key=\"plugin_port\" value=\"${TC_PLUGIN_PORT}\" \/>
+    /<appSettings>/a \    <add key=\"ac_server_port\" value=\"${TC_LOCAL_PORT}\" \/>
+  " AC_TrackCycle_Console.exe.config
   cp -rf . $TRACK_CYCLE_PATH
 fi
