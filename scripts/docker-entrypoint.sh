c5c33cf70e01fd6f776ec1ddc785953df2af4885
germanrcuriel <germix@germix.net>
2018-02-27 16:50:58 +0100

    Run all plugins before acServer

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,5 +4,14 @@ export INSTALL_PATH="/steamapps/${ASSETTO_FOLDER}"
 
 install-all.sh
 
+cd $INSTALL_PATH/plugins/stracker
+nohup ./stracker_linux_x86/stracker --stracker_ini stracker.ini >/dev/null 2>&1 &
+
+cd $INSTALL_PATH/plugins/minorating
+nohup mono ./MinoRatingPlugin.exe >/dev/null 2>&1 &
+
+cd $INSTALL_PATH/plugins/track-cycle
+nohup mono ./AC_TrackCycle_Console.exe >/dev/null 2>&1 &
+
 cd $INSTALL_PATH
 ./acServer
