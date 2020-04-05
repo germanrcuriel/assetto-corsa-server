df7cc54c35bfda6d079eeaa9b7a67a8276987b20
germanrcuriel <germix@germix.net>
2020-04-05 19:33:01 +0200

    Fix udp2ws@0.4.0 installation

--- scripts/install-udp2ws.sh
+++ scripts/install-udp2ws.sh
@@ -10,12 +10,12 @@ if [ ! -d $UDP2WS_PATH ]; then
     s/proxyPluginPort =.*/proxyPluginPort = ${UW_PROXY_PLUGIN_PORT}/
     s/sendPort =.*/sendPort = ${UW_LOCAL_PORT}/
     s/receivePort =.*/receivePort = ${UW_PLUGIN_PORT}/
-    s/enabled =.*/enabled = ${UW_REDIS_ENABLED}/
-    s/host =.*/host = ${UW_REDIS_HOST}/
-    s/port = 6379/port = ${UW_REDIS_PORT}/
-    s/password =.*/password = ${UW_REDIS_PASSWORD}/
-    s/publishChannel =.*/publishChannel = ${UW_REDIS_PUBLISH_CHANNEL}/
-    s/subscribeChannel =.*/subscribeChannel = ${UW_REDIS_SUBSCRIBE_CHANNEL}/
+    s/redisEnabled =.*/redisEnabled = ${UW_REDIS_ENABLED}/
+    s/redisHost =.*/redisHost = ${UW_REDIS_HOST}/
+    s/redisPort =.*/redisPort = ${UW_REDIS_PORT}/
+    s/redisPassword =.*/redisPassword = ${UW_REDIS_PASSWORD}/
+    s/redisPublishChannel =.*/redisPublishChannel = ${UW_REDIS_PUBLISH_CHANNEL}/
+    s/redisSubscribeChannel =.*/redisSubscribeChannel = ${UW_REDIS_SUBSCRIBE_CHANNEL}/
     s/; password =.*/password = ${UW_PASSWORD}/
     s/port =.*/port = ${UW_PORT}/
   " udp2ws.ini
